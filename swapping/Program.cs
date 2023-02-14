31db7f8f8117a608288fbbde02c6a4d8bf51afe7
Ajal Jose <ajal.jose@experionglobal.local>
2023-02-14 10:43:27 +0530

    2 commit

--- swapping/Program.cs
+++ swapping/Program.cs
@@ -4,10 +4,12 @@ public class SwapExample
     public static void Main(string[] args)
     {
         int a = 5, b = 10;
-        Console.WriteLine("Before swap a= " + a + " b= " + b);
+        Console.WriteLine("Before swap a: " + a + " b= " + b);
         a = a * b;      
         b = a / b;       
         a = a / b;     
-        Console.Write("After swap a= " + a + " b= " + b);
+        Console.Write("After swap a: " + a + " b= " + b);
+        Console.WriteLine("Bye");
     }
+
 }
